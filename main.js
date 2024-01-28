909fe99ef996372ee3f68e450213dfeff20c98e5
Samatsss <144174632+Samatsss@users.noreply.github.com>
2024-01-28 18:14:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+// 1.1
+// let num = 3
+// alert(num)
+
+
+//1.2
+// let a = 10
+// let b = 2
+// alert(a + b)
+// alert(a - b)
+// alert(a * b)
+// alert(a / b)
+
+
+//1.3
+// let c = 15
+// let d = 2
+// result = c + d
+// alert(result)
+
+
+//1.4
+// let a = 10
+// let b = 2
+// let c = 5
+// alert(a + b + c)
+
+
+//1.5
+// let a = 17
+// let b = 10
+// let c
+//     c = a - b
+// let d = 7
+// let result
+// result = c + d
+// alert(result)
+
+
+//2.1
+// let str
+// str = 'Привет,Мир!'
+// alert(str)
+
+
+//2.2
+// let str1= 'Привет'
+// let str2= 'Мир!'
+// alert(str1 + str2)
+
+
+//2.3
+// let name = "%Samat%"
+// alert('Привет ' + name + '!')
+
+
+//2.4
+// let age = '%16%'
+// alert('Мне ' + age + ' лет!')
+
+
+//3.1
+// let name = prompt('Как тебя зовут?');
+// alert(`Ваше имя ${name}`)
+
+
+//3.2
+// let num = prompt('Спрашиваю у вас число');
+// alert(`Ваше число в квадрате ${num*num}`)
+
+
+//4.1
+// let str = 'abcde';
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[4]);
+
+
+//4.2
+// let num = '12345';
+// let product = 1;
+// let digits = num.split('');
+// for (let i = 0; i < digits.length; i++) {
+//   product *= parseInt(digits[i], 10);
+// }
+// alert('Произведение цифр числа: ' + product);
+
+
+//5.1
+// month = prompt('Сколько месяцев?') // ну месяц примерно 30 дней
+// day = prompt('Сколько дней?')
+// hour = prompt('Сколько часов?')
+// vivod = month*2592000 + day*86400 + hour*3600
+// alert(`Вот столько секунд ${vivod}`)
+
+
+// 5.2
+// hour = prompt('Укажите час:')
+// minute = prompt('Укажите минуты:')
+// second = prompt('Укажите секунды:')
+// alert(`Вы удивитесь, но ваше время ${hour + ':' + minute + ':' + second}`)
+
+//5.3
+// let a = 56
+// alert(a*a)
+
+
+
